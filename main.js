03da1a645bfc8b4c679b6f653db1069a02ddcce3
allybaba <allylavanda@aol.com>
2022-12-15 11:40:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+const prefix = '-';
+
+client.once('ready', () => {
+    console.log('WeebBot is online!');
+});
+
+client.on('messageCreate', message => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    }
+});
+
+client.login(token);
